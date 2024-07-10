2355f7b30d4396679f3ceb17e14f21966b45cdfb
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-07-10 14:12:50 +0700

    problem solved

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,16 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  let resultCollect = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        resultCollect.push(i);
+        resultCollect.push(j);
+        return resultCollect;
+      }
+    }
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([2, 7, 11, 15], 23));
